f7563c2d3633941396fa57c6c2f6af2f93bf67df
항준 <hanggjun@hangjun-ui-MacBook-Pro.local>
2011-05-26 20:21:24 +0900

    Set up 4-tab-view

---
+++ Nodazi/SettingViewController.h
@@ -0,0 +1,16 @@
+//
+//  SettingViewController.h
+//  Nodazi
+//
+//  Created by 항준 on 11. 5. 26..
+//  Copyright 2011 서울대학교. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+
+@interface SettingViewController : UIViewController {
+    
+}
+
+@end
