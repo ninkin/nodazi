73f05cbee65f9e089057a3c415d75d9e2a6246dd
ninkin <flytothemoon@gmail.com>
2011-06-14 17:16:51 +0900

    핀을 꼽습니다

---
+++ Nodazi/PlaceMarker.h
@@ -0,0 +1,17 @@
+//
+//  PlaceMarker.h
+//  Nodazi
+//
+//  Created by STCube1 on 11. 6. 14..
+//  Copyright 2011 서울대학교. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+
+@interface PlaceMarker : NSObject {
+@private
+    
+}
+
+@end
---
+++ Nodazi/PlaceMarker.m
@@ -0,0 +1,29 @@
+//
+//  PlaceMarker.m
+//  Nodazi
+//
+//  Created by STCube1 on 11. 6. 14..
+//  Copyright 2011 서울대학교. All rights reserved.
+//
+
+#import "PlaceMarker.h"
+
+
+@implementation PlaceMarker
+
+- (id)init
+{
+    self = [super init];
+    if (self) {
+        // Initialization code here.
+    }
+    
+    return self;
+}
+
+- (void)dealloc
+{
+    [super dealloc];
+}
+
+@end
